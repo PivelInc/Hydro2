551e9bb117f1576682363d8926d85de46ecdc93c
chaosinnovator <thomas.boland@outlook.com>
2025-07-17 23:15:49 -0700

    Test

--- app/Pivel/Hydro2/Services/Entity/EntityRepository.php
+++ app/Pivel/Hydro2/Services/Entity/EntityRepository.php
@@ -432,7 +432,6 @@ class EntityRepository implements IEntityRepository
             // check if pkValue is an object representing another Entity, and if so get the value of it's primary key.
             // TODO this should be recursive as long as pkValue represents another Entity
             if (is_object($pKValue) && count((new ReflectionClass(get_class($pKValue)))->getAttributes(Entity::class)) == 1) {
-                var_dump($pKValue);
                 // Has an Entity tag
                 $definition = new EntityDefinition(get_class($pKValue));
                 $fieldName = $definition->GetPrimaryKeyField()->FieldName;
