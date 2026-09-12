719cec1c2b341f38d4d37f389b71fbde08bedeee
coderient <thomas.boland@outlook.com>
2026-09-11 17:43:33 -0700

    Version bump

--- app/Pivel/Hydro2/Views/BaseWebView.php
+++ app/Pivel/Hydro2/Views/BaseWebView.php
@@ -7,9 +7,9 @@ use Pivel\Hydro2\Extensions\RequireStyle;
 use Pivel\Hydro2\Hydro2;
 use ReflectionClass;
 
-#[RequireScript('/assets/Pivel/Hydro2/hydro2-2.3.1.0.js', Inline: false)]
+#[RequireScript('/assets/Pivel/Hydro2/hydro2-2.4.0.0.js', Inline: false)]
 #[RequireScript('/h2_modules/Pivel/Hydro2/H2View.js', IsModule: true, Inline: false)]
-#[RequireStyle('/assets/Pivel/Hydro2/hydro2-2.3.1.0.css', Inline: false)]
+#[RequireStyle('/assets/Pivel/Hydro2/hydro2-2.4.0.0.css', Inline: false)]
 class BaseWebView extends BaseView
 {
     protected $Scripts = '';
